1d36b29ea826ea2c1432a173d1eac3b226519536
petertran98 <Petertran198@gmail.com>
2022-07-01 14:23:55 -0700

    Created structural conditional format form for log in and log out

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
+  isLoggedIn: Boolean;
+  constructor(private accountService: AccountService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.accountService.login(this.model).subscribe(
+      (accountInfo) => {
+        console.log(accountInfo);
+        this.isLoggedIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  logout() {
+    this.isLoggedIn = false;
+  }
 }
