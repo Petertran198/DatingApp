e49f9c790a8de3f73208ddf3031c266c295f346c
Peter Tran <Petertran198@gmail.com>
2023-08-05 15:26:01 -0700

    Inital commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+
+        IActionResult result = null;
+            try
+            {
+                var q = await _context.Users.ToListAsync();
+                result = Ok(q);
+            }
+            catch (System.Exception ex)
+            {
+                result = BadRequest(ex.Message);
+            }
+            
+            return result;
+    }
+    
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        IActionResult result = null; 
+        try {
+         var q = await _context.Users.FindAsync(id);
+         result = Ok(q);
+
+        }catch(System.Exception ex){
+
+            result = BadRequest(ex.Message);
+        }
+        return result;
+    }
+}
